3eadc7168f7643e75c08d4bf5bb8ebc2947b5576
medalha01 <hiperboss2009@gmail.com>
2024-07-02 16:52:19 -0300

    feat: Adding validation to drug dto

--- src/medi-test/drugs.dto.ts
+++ src/medi-test/drugs.dto.ts
@@ -1,7 +1,23 @@
+//drugs.dto.ts
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
 export class DrugDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   manufacturer: string;
+
+  @IsInt()
   buyCost: number;
+
+  @IsString()
+  @IsNotEmpty()
   composition: string;
 }
