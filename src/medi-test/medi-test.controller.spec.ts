a4bd294d4a7abd6d84211048f0cb5a7ef3f5e9f5
medalha01 <hiperboss2009@gmail.com>
2024-07-03 01:17:18 -0300

    Feat: Adding medi-test controller unit tests

--- src/medi-test/medi-test.controller.spec.ts
+++ src/medi-test/medi-test.controller.spec.ts
@@ -1,15 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MediTestController } from './medi-test.controller';
+import { MediTestService } from './medi-test.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { DrugDto, StorageDto, DRDto } from './drugs.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('MediTestController', () => {
   let controller: MediTestController;
+  let service: MediTestService;
+
+  const mockMediTestService = {
+    createDrug: jest.fn(),
+    getDrugs: jest.fn(),
+    getDrugById: jest.fn(),
+    getDrugByName: jest.fn(),
+    getDrugByComposition: jest.fn(),
+    updateDrug: jest.fn(),
+    deleteDrug: jest.fn(),
+    getStorage: jest.fn(),
+    getStorageByDrugId: jest.fn(),
+    addToStorage: jest.fn(),
+    getStorageTransactions: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MediTestController],
+      providers: [
+        {
+          provide: MediTestService,
+          useValue: mockMediTestService,
+        },
+      ],
     }).compile();
 
     controller = module.get<MediTestController>(MediTestController);
+    service = module.get<MediTestService>(MediTestService);
   });
 
   it('should be defined', () => {
