9914d2569e0f9ae83bc1ba3899dd7f219ea7ab53
medalha01 <hiperboss2009@gmail.com>
2024-07-02 21:38:54 -0300

    Feat: Fully working authentication + running locally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediTestModule } from './medi-test/medi-test.module';
@@ -7,7 +8,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MediTestModule, PrismaModule, AuthModule],
+  imports: [
+    MediTestModule,
+    PrismaModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/medi-test/medi-test.module.ts
+++ src/medi-test/medi-test.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MediTestController } from './medi-test.controller';
 import { MediTestService } from './medi-test.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MediTestController],
-  providers: [MediTestService]
+  providers: [MediTestService],
 })
 export class MediTestModule {}
